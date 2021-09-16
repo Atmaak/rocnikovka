922c0f76cf7e3677eb775ddfdaa12258dacc19d0
Atmaak <kubjak21@gmail.com>
2021-09-16 20:18:30 +0200

    Update list.js

--- backend/script/list.js
+++ backend/script/list.js
@@ -21,7 +21,11 @@ con.connect();// pripoji se k db
 
 var sql
 
-addItem('kokot', 1, 6, 45)
+//addItem('fasga', 1, 6, 45)
+
+displayNewestList(1)
+
+//createList(1)
 
 function createList(id_uzi, res) {
   sql = `INSERT INTO seznamy(id_uzi) VALUES ("${id_uzi}")`
@@ -42,11 +46,7 @@ function addItem(item, id_sta, id_sez, kusy) {
 
   con.query(`SELECT id_pol FROM polozky WHERE nazev = "${item}"`, function (err, result) {
     if(err) throw err
-    if(result.length == 0){
-      sql = `INSERT INTO pol_sez(id_sez, id_pol, kusy, id_sta) VALUES (${id_sez}, ${result[result.length].id_pol}, ${kusy}, ${id_sta})`
-    }else{
-      sql = `INSERT INTO pol_sez(id_sez, id_pol, kusy, id_sta) VALUES (${id_sez}, ${result[result.length-1].id_pol}, ${kusy}, ${id_sta})`
-    }
+    sql = `INSERT INTO pol_sez(id_sez, id_pol, kusy, id_sta) VALUES (${id_sez}, ${result[result.length-1].id_pol}, ${kusy}, ${id_sta})`
     console.log(sql)
     
     con.query(sql, function (err, result) {
@@ -54,7 +54,23 @@ function addItem(item, id_sta, id_sez, kusy) {
     })
 
   })
-
-  //sql = `INSERT INTO pol_sez(id_sez, id_pol, kusy, id_sta) VALUES (${id_sez}, ${id_pol}, ${kusy}, ${id_sta})`
-
 }
+
+function displayNewestList(id_uzi) {
+  sql = `SELECT * FROM seznamy WHERE id_uzi = ${id_uzi}`
+
+  con.query(sql, function (err, result) {
+    //console.log(result)
+    sortByDate(result)
+    console.log(result[result.length-1].id_sez)
+   
+  })
+}
+
+
+const sortByDate = arr => {
+   const sorter = (a, b) => {
+      return new Date(a.date).getTime() - new Date(b.date).getTime();
+   }
+   arr.sort(sorter);
+};
