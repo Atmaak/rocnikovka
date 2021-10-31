401e436cc8438603a6c1b7dcc7060154a78749fc
Atmaak <kubjak21@gmail.com>
2021-10-31 14:29:39 +0100

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -5,20 +5,22 @@ const Login = ({ onSubmit, onClickShowRegister }) => {
   var refPassword = useRef();
   return (
     <>
-    <form onSubmit={e => e.preventDefault()}>
-      <div>
-        <label>Username</label>
-        <input type="text" placeholder="username" ref={refUsername} />
-      </div>
-      <div>
-        <label>Password</label>
-        <input type="password" placeholder="password" ref={refPassword} />
-      </div>
-      <div>
-        <input type='submit' onClick={async () => await onSubmit(refUsername.current.value, refPassword.current.value)} />
-      </div>
-    </form>
-    <p>Not registered? <span onClick={() => {onClickShowRegister(true)}} className='link'>Click here</span></p>
+    <div>
+      <form onSubmit={e => e.preventDefault()}>
+        <div>
+          <label>Username</label>
+          <input type="text" placeholder="username" ref={refUsername} />
+        </div>
+        <div>
+          <label>Password</label>
+          <input type="password" placeholder="password" ref={refPassword} />
+        </div>
+        <div>
+          <input type='submit' onClick={async () => await onSubmit(refUsername.current.value, refPassword.current.value)} />
+        </div>
+      </form>
+      <p>Not registered? <span onClick={() => {onClickShowRegister(true)}} className='link'>Click here</span></p>
+    </div>
     </>
   );
 };
