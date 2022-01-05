ffb0858643c58d8116663edeab3625373d4e91b5
Atmaak <kubjak21@gmail.com>
2022-01-05 17:57:12 +0100

    xdd

--- backend/script/list.js
+++ backend/script/list.js
@@ -2,10 +2,10 @@ const mysql = require("mysql");
 const dotenv = require("dotenv").config();
 
 const con = mysql.createConnection({
-  host: 'localhost',
-  user: 'atmaak',
-  password: "kokotkokot",
-  database: 'rocnikovka',
+  host: process.env.db_host,
+  user: process.env.db_user,
+  password: "",
+  database: process.env.db_name,
 });
 
 
--- backend/script/login.js
+++ backend/script/login.js
@@ -3,10 +3,10 @@ const passwordHash = require("password-hash");
 const dotenv = require("dotenv").config();
 
 const con = mysql.createConnection({
-  host: 'localhost',
-  user: 'atmaak',
-  password: "kokotkokot",
-  database: 'rocnikovka',
+  host: process.env.db_host,
+  user: process.env.db_user,
+  password: "",
+  database: process.env.db_name,
 });
 
 module.exports = {
