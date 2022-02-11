9047d50df25fe11b24b3bc3fdcd6bb4df248508f
Martin "Atmaak" Kubjak <77574694+Atmaak@users.noreply.github.com>
2022-02-11 14:50:49 +0100

    xd

--- ostatni/rocnikovka (2).sql
+++ ostatni/rocnikovka (2).sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Počítač: 127.0.0.1
--- Vytvořeno: Čtv 10. úno 2022, 20:43
--- Verze serveru: 10.4.20-MariaDB
--- Verze PHP: 7.3.29
+-- Vytvořeno: Pát 11. úno 2022, 14:49
+-- Verze serveru: 10.4.13-MariaDB
+-- Verze PHP: 7.4.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,8 +24,19 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Zástupná struktura pro pohled `adminrodiny`
+-- (See below for the actual view)
+--
+CREATE TABLE `adminrodiny` (
+`id_uzi` int(11)
+,`id_hla` int(11)
+);
+
+-- --------------------------------------------------------
+
+--
 -- Zástupná struktura pro pohled `items`
--- (Vlastní pohled viz níže)
+-- (See below for the actual view)
 --
 CREATE TABLE `items` (
 `nazev` varchar(64)
@@ -214,7 +225,17 @@ CREATE TABLE `uzivatele` (
 INSERT INTO `uzivatele` (`id_uzi`, `id_opr`, `id_fam`, `jmeno`, `email`, `heslo`) VALUES
 (1, 1, 1, 'admin', 'admin@admin.admin', 'sha1$e2b87d22$1$ded12fd3dadbd39ae86b95aabaf899ca46b097d8'),
 (17, 2, 1, 'atmaak', 'kubjak21@gmail.com', 'sha1$cb90a39f$1$9fb38f614dad5a6e56be345f69734d4fc16ca268'),
-(18, 2, 0, 'xdd', 'xdd@xdd.xdd', 'sha1$ae9bced2$1$32426f36a8adc808baf00e2dc575000e08651fe6');
+(18, 2, 0, 'xdd', 'xdd@xdd.xdd', 'sha1$ae9bced2$1$32426f36a8adc808baf00e2dc575000e08651fe6'),
+(19, 2, 0, 'honza', 'dasdas@dasdascacca.casca', 'sha1$442cd053$1$588ecaefc15ac7d29930a63b8b2fe8d1b786fe34');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura pro pohled `adminrodiny`
+--
+DROP TABLE IF EXISTS `adminrodiny`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `adminrodiny`  AS  select `uzivatele`.`id_uzi` AS `id_uzi`,`rodiny`.`id_hla` AS `id_hla` from (`uzivatele` join `rodiny` on(`uzivatele`.`id_fam` = `rodiny`.`id_fam`)) ;
 
 -- --------------------------------------------------------
 
@@ -223,33 +244,33 @@ INSERT INTO `uzivatele` (`id_uzi`, `id_opr`, `id_fam`, `jmeno`, `email`, `heslo`
 --
 DROP TABLE IF EXISTS `items`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `items`  AS SELECT `polozky`.`nazev` AS `nazev`, `pol_sez`.`kusy` AS `kusy`, `pol_sez`.`id_sez` AS `id_sez`, `pol_sez`.`id_pol` AS `id_pol`, `stavy`.`id_sta` AS `id_sta`, `polozky`.`id_szn` AS `id_szn`, `serazeni`.`nazev` AS `nazevSerazeni`, `stavy`.`nazev` AS `stav` FROM (((`pol_sez` join `polozky` on(`pol_sez`.`id_pol` = `polozky`.`id_pol`)) join `stavy` on(`pol_sez`.`id_sta` = `stavy`.`id_sta`)) join `serazeni` on(`polozky`.`id_szn` = `serazeni`.`id_szn`)) ;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `items`  AS  select `polozky`.`nazev` AS `nazev`,`pol_sez`.`kusy` AS `kusy`,`pol_sez`.`id_sez` AS `id_sez`,`pol_sez`.`id_pol` AS `id_pol`,`stavy`.`id_sta` AS `id_sta`,`polozky`.`id_szn` AS `id_szn`,`serazeni`.`nazev` AS `nazevSerazeni`,`stavy`.`nazev` AS `stav` from (((`pol_sez` join `polozky` on(`pol_sez`.`id_pol` = `polozky`.`id_pol`)) join `stavy` on(`pol_sez`.`id_sta` = `stavy`.`id_sta`)) join `serazeni` on(`polozky`.`id_szn` = `serazeni`.`id_szn`)) ;
 
 --
--- Indexy pro exportované tabulky
+-- Klíče pro exportované tabulky
 --
 
 --
--- Indexy pro tabulku `markety`
+-- Klíče pro tabulku `markety`
 --
 ALTER TABLE `markety`
   ADD PRIMARY KEY (`id_mark`);
 
 --
--- Indexy pro tabulku `opravneni`
+-- Klíče pro tabulku `opravneni`
 --
 ALTER TABLE `opravneni`
   ADD PRIMARY KEY (`id_opr`);
 
 --
--- Indexy pro tabulku `polozky`
+-- Klíče pro tabulku `polozky`
 --
 ALTER TABLE `polozky`
   ADD PRIMARY KEY (`id_pol`),
   ADD KEY `id_szn` (`id_szn`);
 
 --
--- Indexy pro tabulku `pol_sez`
+-- Klíče pro tabulku `pol_sez`
 --
 ALTER TABLE `pol_sez`
   ADD KEY `id_sez` (`id_sez`),
@@ -257,19 +278,19 @@ ALTER TABLE `pol_sez`
   ADD KEY `id_sta` (`id_sta`);
 
 --
--- Indexy pro tabulku `rodiny`
+-- Klíče pro tabulku `rodiny`
 --
 ALTER TABLE `rodiny`
   ADD PRIMARY KEY (`id_fam`);
 
 --
--- Indexy pro tabulku `serazeni`
+-- Klíče pro tabulku `serazeni`
 --
 ALTER TABLE `serazeni`
   ADD PRIMARY KEY (`id_szn`);
 
 --
--- Indexy pro tabulku `seznamy`
+-- Klíče pro tabulku `seznamy`
 --
 ALTER TABLE `seznamy`
   ADD PRIMARY KEY (`id_sez`),
@@ -277,13 +298,13 @@ ALTER TABLE `seznamy`
   ADD KEY `id_fam` (`id_fam`);
 
 --
--- Indexy pro tabulku `stavy`
+-- Klíče pro tabulku `stavy`
 --
 ALTER TABLE `stavy`
   ADD PRIMARY KEY (`id_sta`);
 
 --
--- Indexy pro tabulku `uzivatele`
+-- Klíče pro tabulku `uzivatele`
 --
 ALTER TABLE `uzivatele`
   ADD PRIMARY KEY (`id_uzi`),
@@ -339,7 +360,7 @@ ALTER TABLE `stavy`
 -- AUTO_INCREMENT pro tabulku `uzivatele`
 --
 ALTER TABLE `uzivatele`
-  MODIFY `id_uzi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id_uzi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- Omezení pro exportované tabulky
