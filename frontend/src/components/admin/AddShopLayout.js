5ad2ace8c483c473992ddd9ac0ae80e93a3e5a87
Martin "Atmaak" Kubjak <77574694+Atmaak@users.noreply.github.com>
2022-04-20 08:31:21 +0200

    Update AddShopLayout.js

--- frontend/src/components/admin/AddShopLayout.js
+++ frontend/src/components/admin/AddShopLayout.js
@@ -1,14 +1,52 @@
 import React, { useState, useEffect, useRef } from 'react'
 
 import fetch from 'node-fetch'
+import Dropdown from "react-dropdown";
+import "react-dropdown/style.css";
 
 const AddShopLayout = () => {
+  const [typy, setTypy] = useState();
+  const [shopy, setShopy] = useState()
+
+  const drop = useRef()
+
+  useEffect(() =>{
+    const getTypy = async () => {
+      const res = await fetch('http://localhost:3001/item/types')
+      const data = await res.json()
+      await setTypy(data)
+    }
+    
+    const getShops = async () => {
+      var xd = []
+      const res = await fetch('http://localhost:3001/type/getShops')
+      const data = await res.json()
+      for (let i = 0; i < data.length; i++) {
+        xd[i] = { value: data[i].nazev, id_mark: data[i].id_mark };
+      }
+      await setShopy(xd)
+    }
+
+    getShops()
+    getTypy()
+    console.log(shopy)
+    console.log(typy)
+
+
+
+  }, [])
 
 
  
   return (
     <div className="AddShopLayout">
+      <div><Dropdown
+          options={shopy}
+          placeholder="Select type of list"
+          ref={drop}
+        /></div>
 
+        
     </div>
   )
 }
