133bb729c600f75dede8fddacd428186f185c453
Atmaak <kubjak21@gmail.com>
2022-06-12 21:01:48 +0200

    xd

--- frontend/src/components/admin/AdminPanel.js
+++ frontend/src/components/admin/AdminPanel.js
@@ -3,7 +3,6 @@ import EditLayout from './EditLayout'
 import ShopLayout from './ShopLayout'
 
 const AdminPanel = () => {
-  const [showShopLayout, setShowShopLayout] = useState(true)
   
   return <>
   <h1>Admin Panel</h1>
